4ea32acbe68c57f36bc741d0f965e72d9692b77c
Prajukta <prajuktaha@gmail.com>
2026-06-25 00:02:52 +0530

    check if areanis greater than the perimeter of rectangle comparison

--- areaperimetercomaprision.java
+++ areaperimetercomaprision.java
@@ -1,12 +1,19 @@
 //compare if area is greater than perimeter of the rectangle
 import java.util.Scanner;
 public class areaandperimeterofrectangle {
-    public static void main(System.in){
+    public static void main(String [] args){
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter the length of the rectangle : ");
         double length = sc.nextDouble();
         System.out.print("Enter the breadth of the rectangle : ");
         double breadth = sc.nextDouble();
-        
+        double area = length * breadth;
+        double perimeter = 2 * (length + breadth);
+        if(area > perimeter){
+            System.out.println("Yes area is greater than perimeter");
+        }
+        else{
+            System.out.println("no");
+        }
     }
 }
