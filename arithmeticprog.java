70fc437c7049242e5d1d14487f16a9b5c487814b
Prajukta <prajuktaha@gmail.com>
2026-07-06 23:57:49 +0530

    arithmatic progression of a num

---
+++ arithmeticprog.java
@@ -0,0 +1,8 @@
+//display AP 2 , 5 , 8 ,11 upto n terms
+import java.util.Scanner;
+public class arprogression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
