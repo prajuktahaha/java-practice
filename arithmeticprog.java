639765d6463d00f61fb41d8177ea9d3b86e31b45
Prajukta <prajuktaha@gmail.com>
2026-07-07 23:48:43 +0530

    arithmetic progression of number

--- arithmeticprog.java
+++ arithmeticprog.java
@@ -2,7 +2,13 @@
 import java.util.Scanner;
 public class arprogression {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
+        Scanner sc  = new Scanner(System.in);
+        int [] arr = {2 , 5 , 8 , 11};
+        System.out.println("how many n terms : ");
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n && i < arr.length ; i++){
+            System.out.print(a+i*d);
+        }
+        System.out.println(arr[i] + " ");
     }
 }
