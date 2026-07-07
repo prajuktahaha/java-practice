c063fe2179fb01fb97c31860765cc3d2c657c56d
Prajukta <prajuktaha@gmail.com>
2026-07-08 00:10:14 +0530

    arithmetic progression of number

--- arithmeticprog.java
+++ arithmeticprog.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class arprogression {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
-        int [] arr = {2 , 5 , 8 , 11};
-        System.out.println("how many n terms : ");
+        int[] arr = {2 , 5 , 8 , 11};
+        System.out.println(" enter the 'n'th term : ");
         int n = sc.nextInt();
-        for(int i = 0 ; i < n && i < arr.length ; i++){
-            System.out.print(a+i*d);
-        }
-        System.out.println(arr[i] + " ");
+        int a = arr[0];
+        int d = arr[1] - arr[0];
+        int AP = a + (n - 1) * d;
+        System.out.println("for the" + n + "th term the AP is" + AP);
     }
 }
+//ok i did it very easy way u can do it better its noob commo
