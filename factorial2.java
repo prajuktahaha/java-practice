12236182ed0fbf0eb33e9101859b1d905761073e
Prajukta <prajuktaha@gmail.com>
2026-07-10 23:43:32 +0530

    Add factorial calculation in factorial2.java

---
+++ factorial2.java
@@ -0,0 +1,10 @@
+public class factorial {
+    public static void main(String [] args ){
+        int n = 10;
+        int f = 1;
+        for(int i = 1 ; i <= n ;i++){
+            f *= i;
+        }
+        System.out.println(f);
+    }
+}
