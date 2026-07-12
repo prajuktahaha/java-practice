970ad30defa41aef5549ec6eff0bd53846d202d6
Prajukta <prajuktaha@gmail.com>
2026-07-13 00:12:35 +0530

    just demo

--- demo.java
+++ demo.java
@@ -4,15 +4,16 @@ public class ss {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array : ");
+        int x = sc.nextInt();
         int [] arr = new int[x];
         System.out.println("Enter the array : ");
-        for(int i = 0 ; i < x ; i--){
+        for(int i = 0 ; i < x ; i++){
             arr[i] = sc.nextInt();
         }
         System.out.println("Enter the n term : ");
         int n = sc.nextInt();
-        int a = sc.nextInt();
-        int d = arr[0]-[1];
+        int a = arr[0];
+        int d = arr[1]-arr[0];
         int AP = a + (n-1) * d;
         System.out.println("for the" + n + "th term the AP is" + AP);
     }
