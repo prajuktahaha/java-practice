83abfec1e43dc1acbcc3a8ce65c4bc6b9e2bbdc9
Prajukta <prajuktaha@gmail.com>
2026-07-14 23:31:43 +0530

    Add LowercaseASCIIvalues  to print ASCII characters

---
+++ LowercaseASCIIvalues.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ss{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number between 97 to 122 : ");
+        int n = sc.nextInt();
+        System.out.println((char)(n));
+    }
+}
