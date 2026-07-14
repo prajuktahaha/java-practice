5fb6ae753aee46a926060bd359b12b1be4f5a281
Prajukta <prajuktaha@gmail.com>
2026-07-14 23:43:25 +0530

    print ASCII values

--- ASCIIvalues.java
+++ ASCIIvalues.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ss{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between 97 to 122 : ");
+        System.out.println("Enter a number between 97 to 122 for a-z or 65 to 90 for A - Z : ");
         int n = sc.nextInt();
         System.out.println((char)(n));
     }
