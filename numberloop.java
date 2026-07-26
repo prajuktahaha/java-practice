42bb5e0b42c6c177b9fc531eab4812ad1d651173
Prajukta <prajuktaha@gmail.com>
2026-07-27 02:20:25 +0530

    prinnt loop of n num ex:- n=3 prints 000/ln111/n222/n333...

---
+++ numberloop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class numloop {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        for(int i = 0 ; i<=n ; i++){
+            for(int j = 0 ; j<=n ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
