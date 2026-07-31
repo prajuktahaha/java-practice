1c65d04bbbe1eaf10484cd23384dfbc32345540a
Prajukta <prajuktaha@gmail.com>
2026-07-31 23:55:55 +0530

    flipped triangle number pattern

---
+++ numberflippedtriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class starflippedtriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input of n : ");
+        int n = sc.nextInt();
+        int a = n;
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=a ; j++){
+                System.out.print(j);
+            }
+            a--;
+            System.out.println();
+        }
+    }
+}
