36b3871c046c0addbd8c0486a191f232f81293bf
Prajukta <prajuktaha@gmail.com>
2026-08-07 22:34:34 +0530

    using method return no value(void)

---
+++ returnnovalue.java
@@ -0,0 +1,8 @@
+public class methodnoreturn {
+    static void SayHello(){
+        System.out.println("Hello prajukta");
+    }
+    public static void main(String[]args){
+        SayHello();
+    }
+}
