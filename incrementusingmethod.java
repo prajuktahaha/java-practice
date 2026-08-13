3f8cbf9bf68b5f99d70608d03336ac8068f9f963
Prajukta <prajuktaha@gmail.com>
2026-08-13 21:00:22 +0530

    Refactor increment method to use user input
    Refactor increment method to take user input and display incremented value.

--- incrementusingmethod.java
+++ incrementusingmethod.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 public class incrementusingmethod{
-    public static void increment(int x){
-        x++;
-        System.out.println(x);
-    }
     public static void main(String[] args) {
-        int x = 1;
-        increment(x);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x : ");
+        int x = sc.nextInt();
+        Increment(x);
+    }
+    public static void Increment(int x){
+        x++;
         System.out.println(x);
     }
 }
