0225983cdf32a4970f2f8c6265a44ebe4e52b283
Prajukta <prajuktaha@gmail.com>
2026-08-15 17:25:16 +0530

    greetings usinng method overloading

---
+++ greetingsmo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class greetingsmo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SayHi();
+        System.out.println("Enter the name u want to greet");
+        String Name = sc.nextLine();
+        System.out.println("Hi " + Name);
+    }
+    public static void SayHi(){
+        System.out.println("Hi");
+    }
+    public static void Name(String a){
+        return;
+    }
+}
