f37c38d5160f3626b1a87bbb0013a625579553bc
Prajukta <prajuktaha@gmail.com>
2026-08-17 23:58:54 +0530

    just a rough work of arrays working

---
+++ arraysrough.java
@@ -0,0 +1,9 @@
+public class arrays {
+    public static void main(String[] args) {
+        int [] numbers = {9 , 5 , 7 , 8 , 6};
+        System.out.println(numbers);
+        for(int i = 0 ; i < numbers.length ; i++){
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
