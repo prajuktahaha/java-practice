f69771798017db3bebb91a65f949b0b9b24d8a97
Prajukta <prajuktaha@gmail.com>
2026-08-31 23:35:01 +0530

    Comparing Strings and outputs

---
+++ stringComparission.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class stringComparission {
+    public static void main(String[]args){
+        String [] a1 = {"A" , "B" , "C"};
+        String [] a2 = {"a" , "b" , "c"};
+        if(a1.equals(a2)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
